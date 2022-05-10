c27f19484e7e93403d4a7297889eb416ba16b7e9
SPhaniAjit <97693016+SPhaniAjit@users.noreply.github.com>
2022-05-10 22:57:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var randomNumber1=Math.random();
 randomNumber1*=6;
 randomNumber1=Math.floor(randomNumber1)+1;
 var randomDiceImage1="dice"+randomNumber1+".png";
-var randomImageSource1="images/"+randomDiceImage1;
+var randomImageSource1=randomDiceImage1;
 document.querySelector(".img1").setAttribute("src",randomImageSource1);
 
 var randomNumber2=Math.random();
 randomNumber2*=6;
 randomNumber2=Math.floor(randomNumber2)+1;
 var randomDiceImage2="dice"+randomNumber2+".png";
-var randomImageSource2="images/"+randomDiceImage2;
+var randomImageSource2=randomDiceImage2;
 document.querySelector(".img2").setAttribute("src",randomImageSource2);
 
 if(randomNumber1>randomNumber2){
