b577f57b41fed7d2387036b98ebba4242b5c8770
SPhaniAjit <97693016+SPhaniAjit@users.noreply.github.com>
2022-05-10 22:33:55 +0530

    Add Website files

---
+++ index.js
@@ -0,0 +1,23 @@
+var randomNumber1=Math.random();
+randomNumber1*=6;
+randomNumber1=Math.floor(randomNumber1)+1;
+var randomDiceImage1="dice"+randomNumber1+".png";
+var randomImageSource1="images/"+randomDiceImage1;
+document.querySelector(".img1").setAttribute("src",randomImageSource1);
+
+var randomNumber2=Math.random();
+randomNumber2*=6;
+randomNumber2=Math.floor(randomNumber2)+1;
+var randomDiceImage2="dice"+randomNumber2+".png";
+var randomImageSource2="images/"+randomDiceImage2;
+document.querySelector(".img2").setAttribute("src",randomImageSource2);
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML="🚩Player 1 Wins!";
+}
+else if(randomNumber2>randomNumber1){
+  document.querySelector("h1").innerHTML="🚩Player 2 Wins!";
+}
+else{
+  document.querySelector("h1").innerHTML="Draw!";
+}
